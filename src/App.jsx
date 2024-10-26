2af085d15e224d628aeaaff46094ad64f3844222
Joasmarc <joasmarc@gmail.com>
2024-10-25 21:28:59 -0500

    Feature recargar usuario

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,13 @@ function App() {
         reset,
     } = useForm()
 
+    const {
+        register:register2,
+        handleSubmit: handleSubmit2,
+        formState: { errors:error2 },
+        reset:reset2,
+    } = useForm()
+
     const onChange = (event) => {
         event.target.value = event.target.value.toUpperCase();
     };
@@ -32,21 +39,42 @@ function App() {
     const onRegistrarUsuario = async(data)=>{
         let tipo = 'success';
         let mensaje = "Usuario Registrado Exitosamente";
-
-        const resultado = await axios.post('http://localhost:3001/api/usuarios/', data);
-        if (resultado.data.statusCode !== 200){
+        try {
+            await axios.post('http://localhost:3001/api/usuarios/', data);
+            reset()
+        } catch (error) {
             tipo = 'error'
-            mensaje = resultado.data.message.join(' - ')
+            mensaje = error.response.data.message
+        } finally {
+            toaster.create({
+                description: mensaje,
+                type: tipo,
+            })
         }
+    } 
 
-        reset()
-        toaster.create({
-            description: mensaje,
-            type: tipo,
-        })
+    const onRecargarUsuario = async(data)=>{
+        let tipo = 'success';
+        let mensaje = "Recarga Ralizada Exitosamente";
+        try {
+            await axios.post('http://localhost:3001/api/recargas/', {
+                ...data,
+                monto: parseFloat(data.monto),
+                descripcion: 'recarga',
+            });
+        } catch (error) {
+                tipo = 'error'
+                mensaje = error.response.data.message
+        } finally {
+            toaster.create({
+                description: mensaje,
+                type: tipo,
+            })
+        }
     } 
 
     const onSubmit = handleSubmit(onRegistrarUsuario)
+    const onSubmit2 = handleSubmit2(onRecargarUsuario)
 
     return (
         <HStack>
@@ -86,8 +114,8 @@ function App() {
                                     </Field>
                                     <Field
                                     label="NOMBRES"
-                                    invalid={!!errors.nombres}
-                                    errorText={errors.nombres?.message}
+                                    invalid={!!errors.nombre}
+                                    errorText={errors.nombre?.message}
                                     >
                                         <Input
                                             {...register("nombre", { 
@@ -122,6 +150,49 @@ function App() {
                                         />
                                     </Field>
                                     <Button type="submit">Registrar</Button>
+                                </Stack>
+                            </form>
+                        </Card.Body>
+                    </Card.Root>
+
+                    <Card.Root width="28%">
+                        <Card.Body gap="2">
+                            <Card.Title mt="2">Recargar Usuario</Card.Title>
+                            <Card.Description>
+                                Curabitur nec odio vel dui euismod fermentum.
+                            </Card.Description>
+                            <form onSubmit={onSubmit2}>
+                                <Stack gap="4" align="flex-start" maxW="sm">
+                                    <Field
+                                    label="DOCUMENTO"
+                                    invalid={!!error2.documento}
+                                    errorText={error2.documento?.message}
+                                    >
+                                        <Input
+                                            {...register2("documento", {
+                                                required: "Documento es obligatorio",
+                                                maxLength:30,
+                                            })}
+                                        />
+                                    </Field>
+                                    <Field
+                                    label="MONTO"
+                                    invalid={!!error2.monto}
+                                    errorText={error2.monto?.message}
+                                    >
+                                        <Input
+                                            {...register2("monto", { 
+                                                required: "El monto es obligatorio",
+                                                maxLength: 10,
+                                                pattern: {
+                                                    value: /^\d+(\.\d{1,2})?$/,
+                                                    message: 'Ingrese un número decimal con hasta 2 decimales'
+                                                }                                 
+                                            })}
+                                        />
+                                    </Field>
+                                    
+                                    <Button type="submit">Recargar</Button>
                                 </Stack>
                             </form>
                         </Card.Body>
